3b74fb79fd366a5de60e5ca9c4dde094e3bd92e2
Imran <alimran1171@gmail.com>
2025-10-26 21:38:53 +0600

    added comment

--- IOModule/App.java
+++ IOModule/App.java
@@ -2,23 +2,27 @@ import java.util.Scanner;
 
 public class App{
     public static void main(String[] args){
-
+        // creating a scanner object
         Scanner scanner = new Scanner(System.in);
 
+        // taking user input
         System.out.print("Enter your userId: ");
         String userId = scanner.nextLine();
 
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
 
+        // Hashing the password
         String hashedPassword = Password.hashPassword(password);
         User user = new User(userId, hashedPassword);
 
+        // storing the user data
         DataStorage.saveData(user);
 
         System.out.print("Enter your password again for check: ");
         String enteredPassword = scanner.nextLine();
 
+        // verifying password
         boolean matched = Password.verifyPassword(enteredPassword, hashedPassword);
 
         if(matched){
@@ -27,6 +31,7 @@ public class App{
         else{
             System.out.println("You've entered wrong password!");
         }
-        
+        // closing the scanner object
+        scanner.close();
     }
 }
--- IOModule/Password.java
+++ IOModule/Password.java
@@ -1,10 +1,12 @@
 import org.mindrot.jbcrypt.BCrypt;
 
 public class Password{
+    // encrypts password
     public static  String hashPassword(String plainPassword){
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
 
+    // verify password
     public static boolean verifyPassword(String enteredPassword, String hashedPassword){
         return BCrypt.checkpw(enteredPassword, hashedPassword);
     }
